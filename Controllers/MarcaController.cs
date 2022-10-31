4cf48adcfcc6037f87d495b59d7ae1190b60b844
jorgeortizc06 <jorgeortizc06@gmail.com>
2022-10-30 22:33:36 -0500

    Add project files.

---
+++ Controllers/MarcaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class MarcaController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Query()
+        {
+            TestContext db = new TestContext();
+            var data = db.Database.SqlQuery<Marca>("spMarcaQuery").ToList();
+
+            return Ok(data);
+        }
+    }
+}
