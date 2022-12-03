56c263bc9eece0c047fcf74c0fec44a431d0353c
jorgeortizc06 <jorgeortizc06@gmail.com>
2022-12-03 18:48:34 -0500

    Add Marca Controller

--- Controllers/MarcaController.cs
+++ Controllers/MarcaController.cs
@@ -13,6 +13,7 @@ namespace WebApplication2.Controllers
         public IHttpActionResult Query()
         {
             TestContext db = new TestContext();
+
             var data = db.Database.SqlQuery<Marca>("spMarcaQuery").ToList();
 
             return Ok(data);
@@ -23,9 +24,40 @@ namespace WebApplication2.Controllers
         {
             TestContext db = new TestContext();
 
-            var data = db.Database.SqlQuery<Marca>("spMarcaQuery {0}", IdMarca).FirstOrDefault();
+            var data = db.Database.SqlQuery<Marca>("spMarcaQuery @IdMarca = {0}", IdMarca).FirstOrDefault();
 
             return Ok(data);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Insert(Marca model)
+        {
+            TestContext db = new TestContext();
+
+            db.Database.ExecuteSqlCommand("spMarcaInsert @Descripcion = {0}, @Activo = {1}",
+               model.Descripcion, model.Activo);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Update(Marca model)
+        {
+            TestContext db = new TestContext();
+            db.Database.ExecuteSqlCommand("spMarcaUpdate @IdMarca = {0}, @Descripcion = {1}, @Activo = {2}",
+              model.IdMarca, model.Descripcion, model.Activo);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Delete(Marca model)
+        {
+            TestContext db = new TestContext();
+
+            db.Database.ExecuteSqlCommand("spMarcaDelete @IdMarca = {0}", model.IdMarca);
+
+            return Ok();
         }
     }
 }
